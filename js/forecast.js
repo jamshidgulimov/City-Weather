b73398203b8cf59ddd3f11b56c6b7ae532ebc0b8
jamshidgulimov <jamshidgulimov21@gmail.com>
2024-03-07 07:28:52 +0500

    City Weather

---
+++ js/forecast.js
@@ -0,0 +1,16 @@
+
+
+const KEY = '8381bde3b01a162ed81d52c0f43bb07d'
+
+const getData = async (city) =>{
+    const base = 'https://api.openweathermap.org/data/2.5/weather'
+    const query = `?q=${city}&units=metric&appid=${KEY}`   
+      loader(true)
+    const req = await fetch(base + query)
+    const data = await req.json()
+      loader(false)
+
+    return data
+}
+
+
